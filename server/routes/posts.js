b2beb61d8c73c70aea6291a5ba86f01a232227da
Greg Hayes <gthelight@gmail.com>
2022-04-17 15:02:42 -0400

    Adds mongoose schema

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,9 @@
 import express from "express";
-import getPosts from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-// router.get("/", (req, res) => {
-//   res.send("<h1>Social App</h1>");
-// });
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
